f83809e21e8a49f15d7d947fa3f93b2c4ce7a8b8
Michael Rook <michael@rook.net.au>
2021-11-15 19:09:55 +1100

    Adjustment

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,13 +1,13 @@
 fn main() {
     for i in 1..=1000000 {
         if i % 15 == 0 {
-            println!("{}", "FizzBuzz");
+            println!("{}: {}", i, "FizzBuzz");
         } else if i % 3 == 0 {
-            println!("{}", "Fizz");
+            println!("{}: {}", i, "Fizz");
         } else if i % 5 == 0 {
-            println!("{}", "Buzz");
+            println!("{}: {}", i, "Buzz");
         } else {
-            println!("{}", i);
+            println!("{}: {}", i, i);
         }
     }
 }
