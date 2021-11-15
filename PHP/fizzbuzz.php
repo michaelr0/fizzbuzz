36e3d8ed2f264e1bab3acf329ac8cb3b79898f17
Michael Rook <michael@rook.net.au>
2021-11-15 19:09:51 +1100

    Adjustment

--- PHP/fizzbuzz.php
+++ PHP/fizzbuzz.php
@@ -1,13 +1,13 @@
 <?php
 
-for ($i = 0; $i <= 1000000; ++$i) {
+for ($i = 1; $i <= 1000000; ++$i) {
     if (0 === $i % 15) {
-        echo 'FizzBuzz'.PHP_EOL;
+        echo "{$i}: FizzBuzz".PHP_EOL;
     } elseif (0 === $i % 3) {
-        echo 'Fizz'.PHP_EOL;
+        echo "{$i}: Fizz".PHP_EOL;
     } elseif (0 === $i % 5) {
-        echo 'Buzz'.PHP_EOL;
+        echo "{$i}: Buzz".PHP_EOL;
     } else {
-        echo $i.PHP_EOL;
+        echo "{$i}: {$i}".PHP_EOL;
     }
 }
